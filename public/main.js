42d74096ca31e06f57bc4c30c2ecde8677b7af40
thefifthdentist <madalena73@gmail.com>
2018-04-29 14:30:40 -0400

    Improvements Many

--- public/main.js
+++
@@ -1,7 +0,0 @@
-var elem = document.querySelector('.sidenav');
-var instance = M.Sidenav.init(elem, options);
-
-
-
-var elem = document.querySelector('.fixed-action-btn');
-var instance = M.FloatingActionButton.init(elem, options);
